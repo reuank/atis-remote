6519d4847043d72b8752a3bf2670d0d5c9e31d07
Leon Knauer <git@leonknauer.com>
2019-07-18 17:49:19 +0200

    Improying syntax with shellcheck

--- atis.sh
+++ atis.sh
@@ -1,17 +1,19 @@
+#!/bin/bash
+
 atis(){
   hostname="atis"
   remotePath="Schreibtisch"
   defaultPrinter="pool-sw3"
 
   # Check if file specified
-  if [ -z ${1} ]
+  if [ -z "${1}" ]
   then
     echo "=> No file name specified. Aborting."
     return
   fi
 
   # Check if correct printer specified
-  if [ -z ${2} ]
+  if [ -z "${2}" ]
   then
     printer=$defaultPrinter
   else
@@ -27,16 +29,15 @@ atis(){
   fi
 
   # Move file to atis account
-  scp $1 $hostname:$remotePath
+  scp "$1" $hostname:$remotePath
 
   # Print file on printer pool-sw3
-  file=$( basename $1 )
-  ssh $hostname "lpr -P $printer $remotePath/$file"
+  file=$( basename "$1" )
 
-  if [ $? -ne 0 ]
+  if ssh $hostname "lpr -P $printer $remotePath/$file"
   then
-    echo "=> Printing failed."
-  else
     echo "=> Printed file '$file' successfully on printer '$printer'."
+  else
+    echo "=> Printing failed."
   fi
 }
